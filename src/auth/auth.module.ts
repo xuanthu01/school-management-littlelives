b0e27da6b0e0c73f6d6bb20453fff1bee7c41f67
thupx <xuanthu404@gmail.com>
2020-05-21 11:37:29 +0700

    fix: "secretOrPrivateKey" has been deprecated

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,18 +5,20 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserRepository } from './user.repository';
+import { JwtStrategy } from './jwt.strategy';
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secretOrPrivateKey: process.env.JWT_SECRET,
+      secret: process.env.JWT_SECRET,
       signOptions: {
         expiresIn: '1d',
       },
     }),
     TypeOrmModule.forFeature([UserRepository]),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+  exports: [JwtStrategy],
 })
 export class AuthModule {}
